9cd3c45c0e855bd9c22b963e4f5b3a822474772a
Emilia Zawisza <emilia.zawisza@wp.pl>
2019-08-03 11:04:04 +0200

    first commit

---
+++ savestudent/src/main/java/pl/sda/savestudent/App.java
@@ -0,0 +1,12 @@
+package pl.sda.savestudent;
+
+import pl.sda.studentmodel.Student;
+
+public class App {
+
+    public static void main(String[] args) {
+        Student
+
+
+    }
+}
---
+++ studentmodel/src/main/java/pl/sda/studentmodel/Student.java
@@ -0,0 +1,7 @@
+package pl.sda.studentmodel;
+
+public class Student {
+    private String name;
+    private String lastname;
+    private Integer index;
+}
