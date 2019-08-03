d8c0ef8ef109d69929ead492231af4f6bc591603
Emilia Zawisza <emilia.zawisza@wp.pl>
2019-08-03 12:26:16 +0200

    koniec

--- savestudent/src/main/java/pl/sda/savestudent/App.java
+++ savestudent/src/main/java/pl/sda/savestudent/App.java
@@ -12,7 +12,7 @@ import java.io.File;
 
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Student student1 = new Student("Waldemar", "Klus", 1);
         Student student2 = new Student("Ewa", "Zyk", 2);
         Student student3 = new Student("Jacek", "Walik", 3);
@@ -20,6 +20,7 @@ public class App {
         Student student5 = new Student("Jan", "Malicz", 5);
 
         List<Student> students = Arrays.asList(student1, student2, student3, student4, student5);
+
         ObjectMapper objectMapper = new ObjectMapper(); // po skopiowaniu groupID dziedziczy z głównego Pom
         try {
             objectMapper.writeValue(new File("student.json"), students);
--- studentmodel/src/main/java/pl/sda/studentmodel/Student.java
+++ studentmodel/src/main/java/pl/sda/studentmodel/Student.java
@@ -1,18 +1,17 @@
 package pl.sda.studentmodel;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Student {
     private String name;
     private String lastname;
     private Integer index;
-
-    public Student(String name, String lastname, Integer index) {
-        this.name = name;
-        this.lastname = lastname;
-        this.index = index;
-    }
 
     public String getName() {
         return name;
